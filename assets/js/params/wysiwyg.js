e04c2609b735f8512824e77a898d59a2f657119e
OlegApanovich <dollar4444@gmail.com>
2025-06-04 16:29:03 +0300

    Code style

--- assets/js/params/wysiwyg.js
+++ assets/js/params/wysiwyg.js
@@ -194,94 +194,91 @@ vc.atts.custom_wysiwyg = {
     },
 
     init: function (param, $el) {
-        function initWysiwyg(container) {
-            var wysiwyg = container.find(".wcp-wysiwyg-container");
 
-            var useTabs = wysiwyg.attr("data-use-tabs") === "true",
-                useMenubar = wysiwyg.attr("data-use-menubar") === "true",
-                useMedia = wysiwyg.attr("data-use-media") === "true",
-                useLink = wysiwyg.attr("data-use-link") === "true",
-                useTextColor = wysiwyg.attr("data-use-textcolor") === "true",
-                useBackground = wysiwyg.attr("data-use-background") === "true",
-                useBlockquote = wysiwyg.attr("data-use-blockquote") === "true",
-                useLists = wysiwyg.attr("data-use-lists") === "true",
-                editorHeight = parseInt(wysiwyg.attr("data-use-height")),
-                useRootBlock = wysiwyg.attr("data-use-rootblock") === "true" ||
-                    (wysiwyg.attr("data-use-rootblock") !== "false" && wysiwyg.attr("data-use-rootblock")),
-                editorId = wysiwyg.find("textarea.wcp-wysiwyg-editor").attr("id"),
-                siteUrl = wysiwyg.attr("data-url-site");
+        var wysiwyg = $el.find(".wcp-wysiwyg-container");
 
-            var visualTab, htmlTab;
+        var useTabs = wysiwyg.attr("data-use-tabs") === "true",
+            useMenubar = wysiwyg.attr("data-use-menubar") === "true",
+            useMedia = wysiwyg.attr("data-use-media") === "true",
+            useLink = wysiwyg.attr("data-use-link") === "true",
+            useTextColor = wysiwyg.attr("data-use-textcolor") === "true",
+            useBackground = wysiwyg.attr("data-use-background") === "true",
+            useBlockquote = wysiwyg.attr("data-use-blockquote") === "true",
+            useLists = wysiwyg.attr("data-use-lists") === "true",
+            editorHeight = parseInt(wysiwyg.attr("data-use-height")),
+            useRootBlock = wysiwyg.attr("data-use-rootblock") === "true" ||
+                (wysiwyg.attr("data-use-rootblock") !== "false" && wysiwyg.attr("data-use-rootblock")),
+            editorId = wysiwyg.find("textarea.wcp-wysiwyg-editor").attr("id"),
+            siteUrl = wysiwyg.attr("data-url-site");
 
-            if (useTabs) {
-                var tabs = wysiwyg.find(".wcp-wysiwyg-tabs");
-                visualTab = tabs.find(".wcp-wysiwyg-visual");
-                htmlTab = tabs.find(".wcp-wysiwyg-html");
-            }
+        var visualTab, htmlTab;
 
-            var plugins = [
-                "wpdialogs", "wptextpattern", "wpview", "wordpress", "wpemoji",
-                "charmap", "hr", "fullscreen", "paste"
-            ];
-
-            if (useMedia) plugins.push("wpeditimage", "image", "media");
-            if (useLists) plugins.push("lists");
-            if (useLink) plugins.push("wplink");
-            if (useTextColor || useBackground) plugins.push("textcolor", "colorpicker");
-
-            var toolbar1 = "bold italic underline strikethrough | " +
-                (useBlockquote ? "blockquote " : "") +
-                "alignleft aligncenter alignright alignjustify " +
-                (useLists ? "| bullist numlist outdent indent " : "") +
-                (useLink ? "| link unlink " : "") +
-                "| undo redo";
-
-            var toolbar2 = "formatselect fontselect fontsizeselect " +
-                (useTextColor || useBackground ? "| " +
-                    (useTextColor ? "forecolor " : "") +
-                    (useBackground ? "backcolor " : "") : "") +
-                "| charmap | removeformat | media image";
-
-            tinymce.init({
-                selector: "textarea#" + editorId,
-                mode: "exact",
-                theme: "modern",
-                skin: "lightgray",
-                content_css: tinyMCE.baseURL + "/skins/wordpress/wp-content.css",
-                plugins: plugins.join(" "),
-                external_plugins: {},
-                menubar: useMenubar && "edit insert view format tools",
-                toolbar1: toolbar1,
-                toolbar2: toolbar2,
-                fontsize_formats: "8px 10px 11px 12px 14px 16px 18px 24px 36px 48px",
-                media_buttons: useMedia,
-                image_advtab: useMedia,
-                height: editorHeight,
-                forced_root_block: useRootBlock,
-                relative_urls: false,
-                remove_script_host: false,
-                document_base_url: siteUrl,
-                setup: function (editor) {
-                    editor.on("init", function () {
-                        if (useTabs) {
-                            htmlTab.removeClass("active");
-                            visualTab.addClass("active");
-                        }
-                    });
-                }
-            });
-
-            if (useTabs) {
-                visualTab.on("click", function () {
-                    switchWysiwygTab("visual", visualTab, htmlTab, editorId);
-                });
-
-                htmlTab.on("click", function () {
-                    switchWysiwygTab("html", visualTab, htmlTab, editorId);
-                });
-            }
+        if (useTabs) {
+            var tabs = wysiwyg.find(".wcp-wysiwyg-tabs");
+            visualTab = tabs.find(".wcp-wysiwyg-visual");
+            htmlTab = tabs.find(".wcp-wysiwyg-html");
         }
 
-        initWysiwyg($el);
+        var plugins = [
+            "wpdialogs", "wptextpattern", "wpview", "wordpress", "wpemoji",
+            "charmap", "hr", "fullscreen", "paste"
+        ];
+
+        if (useMedia) plugins.push("wpeditimage", "image", "media");
+        if (useLists) plugins.push("lists");
+        if (useLink) plugins.push("wplink");
+        if (useTextColor || useBackground) plugins.push("textcolor", "colorpicker");
+
+        var toolbar1 = "bold italic underline strikethrough | " +
+            (useBlockquote ? "blockquote " : "") +
+            "alignleft aligncenter alignright alignjustify " +
+            (useLists ? "| bullist numlist outdent indent " : "") +
+            (useLink ? "| link unlink " : "") +
+            "| undo redo";
+
+        var toolbar2 = "formatselect fontselect fontsizeselect " +
+            (useTextColor || useBackground ? "| " +
+                (useTextColor ? "forecolor " : "") +
+                (useBackground ? "backcolor " : "") : "") +
+            "| charmap | removeformat | media image";
+
+        tinymce.init({
+            selector: "textarea#" + editorId,
+            mode: "exact",
+            theme: "modern",
+            skin: "lightgray",
+            content_css: tinyMCE.baseURL + "/skins/wordpress/wp-content.css",
+            plugins: plugins.join(" "),
+            external_plugins: {},
+            menubar: useMenubar && "edit insert view format tools",
+            toolbar1: toolbar1,
+            toolbar2: toolbar2,
+            fontsize_formats: "8px 10px 11px 12px 14px 16px 18px 24px 36px 48px",
+            media_buttons: useMedia,
+            image_advtab: useMedia,
+            height: editorHeight,
+            forced_root_block: useRootBlock,
+            relative_urls: false,
+            remove_script_host: false,
+            document_base_url: siteUrl,
+            setup: function (editor) {
+                editor.on("init", function () {
+                    if (useTabs) {
+                        htmlTab.removeClass("active");
+                        visualTab.addClass("active");
+                    }
+                });
+            }
+        });
+
+        if (useTabs) {
+            visualTab.on("click", function () {
+                switchWysiwygTab("visual", visualTab, htmlTab, editorId);
+            });
+
+            htmlTab.on("click", function () {
+                switchWysiwygTab("html", visualTab, htmlTab, editorId);
+            });
+        }
     }
 };
