b21a66997a90ec937dd0589b6f9a3ddf241ee610
OlegApanovich <dollar4444@gmail.com>
2025-08-17 11:33:17 +0300

    Fix issue with group param margin #2

--- src/ParamsFunctionality/Grouped.php
+++ src/ParamsFunctionality/Grouped.php
@@ -44,10 +44,16 @@ class Grouped {
 
 		$style = 'style="border-left: 5px solid ' . esc_attr( $color ) . '; ' . $margin . '"';
 
-		$global_style = '<style>.vc_ui-panel-content.vc_properties-list.vc_edit_form_elements {padding: 18px 18px 18px 30px}</style>';
+		$global_style = '<style>.vc_ui-panel-content.vc_properties-list.vc_edit_form_elements {padding: 18px 18px 18px 30px}</style></div>';
 
 		$output = str_replace( 'data-vc-ui-element="panel-shortcode-param"', 'data-vc-ui-element="panel-shortcode-param" ' . $style, $output );
 
-		return $output . $global_style;
+		// Find last </div>.
+		$pos = strrpos( $output, '</div>' );
+		if ( false !== $pos ) {
+			$output = substr_replace( $output, $global_style, $pos, strlen( '</div>' ) );
+		}
+
+		return $output;
 	}
 }
