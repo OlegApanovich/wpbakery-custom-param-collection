a24a1d59d37192e0a5f441d055293766559a2921
OlegApanovich <dollar4444@gmail.com>
2026-06-26 11:30:36 +0300

    fix: #27 add js logic for apply groupped color to params

--- assets/js/params_functionality/grouped.js
+++ assets/js/params_functionality/grouped.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function () {
+	function applyGroupColors( context ) {
+		jQuery( '[data-wcp-group-color]', context ).each( function () {
+			jQuery( this ).css( 'border-left', '5px solid ' + jQuery( this ).data( 'wcp-group-color' ) );
+		} );
+	}
 
+	applyGroupColors( document );
+
+	var observer = new MutationObserver( function ( mutations ) {
+		mutations.forEach( function ( mutation ) {
+			jQuery( mutation.addedNodes ).filter( function () {
+				return this.nodeType === 1;
+			} ).each( function () {
+				applyGroupColors( this.parentNode );
+			} );
+		} );
+	} );
+
+	observer.observe( document.body, { childList: true, subtree: true } );
 });
