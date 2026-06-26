0646b076b5c45785d97b05bc72b2d3e37a6e2c38
OlegApanovich <dollar4444@gmail.com>
2026-06-26 12:11:29 +0300

    fix: #27 apply border only for a first item in a row

--- assets/js/params_functionality/grouped.js
+++ assets/js/params_functionality/grouped.js
@@ -1,11 +1,26 @@
 jQuery(document).ready(function () {
+	function isFirstInRow( el ) {
+		var prev = el.previousElementSibling;
+		return !prev || Math.round( el.getBoundingClientRect().top ) !== Math.round( prev.getBoundingClientRect().top );
+	}
+
 	function applyGroupColors( context ) {
-		jQuery( '[data-wcp-group-color]', context ).each( function () {
-			jQuery( this ).css( 'border-left', '5px solid ' + jQuery( this ).data( 'wcp-group-color' ) );
-		} );
+		jQuery( '[data-wcp-group-color]', context )
+			.filter( function () { return isFirstInRow( this ); } )
+			.each( function () {
+				jQuery( this ).css( 'border-left', '5px solid ' + jQuery( this ).data( 'wcp-group-color' ) );
+			} );
 	}
 
 	applyGroupColors( document );
+
+	var resizeTimer;
+	jQuery( window ).on( 'resize', function () {
+		clearTimeout( resizeTimer );
+		resizeTimer = setTimeout( function () {
+			applyGroupColors( document );
+		}, 100 );
+	} );
 
 	var observer = new MutationObserver( function ( mutations ) {
 		mutations.forEach( function ( mutation ) {
